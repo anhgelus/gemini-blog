c59a653a89800ff30b7d0bfe43d419a3bb9dff89
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-03-08 13:53:46 +0100

    feat(backend): simple gemini server

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gemini-blog
 
 go 1.24
+
+require (
+	git.sr.ht/~sotirisp/go-gemini v0.0.0-20241120232757-f2c5ece7cd6e // indirect
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,76 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"git.sr.ht/~sotirisp/go-gemini"
+	"git.sr.ht/~sotirisp/go-gemini/certificate"
+	"github.com/BurntSushi/toml"
+	"log/slog"
+	"os"
+	"os/signal"
+	"time"
+)
 
+type Config struct {
+	Domain string `toml:"domain"`
+}
+
+var (
+	configFile = "config.toml"
+)
+
+func init() {
+	flag.StringVar(&configFile, "config", configFile, "config file")
+}
+
+func main() {
+	flag.Parse()
+	b, err := os.ReadFile(configFile)
+	if err != nil {
+		slog.Info(configFile)
+		panic(err)
+	}
+	var cfg Config
+	if err = toml.Unmarshal(b, &cfg); err != nil {
+		panic(err)
+	}
+
+	certificates := &certificate.Store{}
+	certificates.Register(cfg.Domain)
+	if err = certificates.Load("certs"); err != nil {
+		panic(err)
+	}
+
+	mux := &gemini.Mux{}
+	mux.Handle("/", gemini.FileServer(os.DirFS("public")))
+
+	server := &gemini.Server{
+		Handler:        gemini.LoggingMiddleware(mux),
+		ReadTimeout:    30 * time.Second,
+		WriteTimeout:   1 * time.Minute,
+		GetCertificate: certificates.Get,
+	}
+
+	// starts
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	errCh := make(chan error)
+	go func() {
+		ctx := context.Background()
+		errCh <- server.ListenAndServe(ctx)
+	}()
+
+	select {
+	case err = <-errCh:
+		slog.Error(err.Error())
+	case <-c:
+		slog.Info("Shutting down...")
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+		if err = server.Shutdown(ctx); err != nil {
+			panic(err)
+		}
+	}
 }
